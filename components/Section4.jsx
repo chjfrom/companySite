03d76da55156f4af6f54baf7e79684f9c18b27bc
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-04 19:19:08 +0900

    Section4 web css 작업완료

--- components/Section4.jsx
+++ components/Section4.jsx
@@ -1,7 +1,33 @@
 export default function Section4() {
   return (
     <div className="inner sectionBox">
-      <h1>section4</h1>
+      <div className="section4Wrap ">
+        <div className="section3Wrap">
+          <div className="section2Tex1">
+            대표 금융플랫폼 <br />
+            <span className="fointBlueText">VEXK</span>
+          </div>
+
+          <div className="contentsList">
+            <div>
+              <p className="num">01</p>
+              <p>실시간 데이터 처리</p>
+            </div>
+            <div>
+              <p className="num">02</p>
+              <p>검증된 데이터 설계</p>
+            </div>
+            <div>
+              <p className="num">03</p>
+              <p>모바일 웹 반응형 처리</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <img src="/vexk.png" />
+        </div>
+      </div>
     </div>
   );
 }
