3e574606578ace3990df7aae808dc47527fa2328
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-04 18:16:07 +0900

    Section1 css작업 완료

--- components/Section1.jsx
+++ components/Section1.jsx
@@ -4,10 +4,20 @@ export default function Section1() {
   return (
     <div className="inner sectionBox">
       <ParticleBg />
-      <div className="section">
-        <div className="section1Tex1">신뢰할 수 있는 금융솔루션</div>
-        <div className="section1Tex2">확인된 전문가들에 의해 개발된</div>
-        <div className="section1Tex3">금융 솔루션으로 비즈니스 성장시켜보세요</div>
+      <div className="section1 ection">
+        <div className="section1Tex1">
+          <p>
+            <span className="fointText">신뢰</span>할 수 있는{' '}
+            <span className="fointText">금융솔루션</span>
+          </p>
+        </div>
+        <div className="section1Tex2">
+          확인된 전문가들에 의해 개발된 <br />
+          금융 솔루션으로 비즈니스 성장시켜보세요
+        </div>
+        <button className="startBtn" type="button">
+          시작하기
+        </button>
       </div>
     </div>
   );
