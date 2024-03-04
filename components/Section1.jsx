118fe2f1036b837363f1edb55eae96778c866196
yiminwook <grs0416@gmail.com>
2024-03-04 14:46:57 +0900

    #MAIN#NEW - emotion

--- components/Section1.jsx
+++ components/Section1.jsx
@@ -1,11 +1,19 @@
+import styled from '@emotion/styled';
 import ParticleBg from './ParticleBg';
+
+const ParticleBgBox = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
 
 export default function Section1() {
   return (
     <div className="section">
-      <div className="particleBgBox">
+      <ParticleBgBox>
         <ParticleBg />
-      </div>
+      </ParticleBgBox>
       <h1>section1</h1>
     </div>
   );
