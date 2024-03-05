6ac0c9064f16dd64d20e86659618d8f5d941b6c9
chjfrom <chjfromn@gmail.com>
2024-03-05 15:12:43 +0900

    sectuion5 컴포넌트 수정

--- components/Section5.jsx
+++ components/Section5.jsx
@@ -3,19 +3,18 @@ import KakaoMap from './KakaoMap';
 
 const Wrap = styled.div`
   box-sizing: border-box;
-  height: 500px;
+  height: 100vh;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   text-align: center;
   padding: 0 28px;
-  margin-bottom: 20px;
 `;
 
 const Top = styled.div`
   box-sizing: border-box;
   width: 100%;
+  margin-bottom: 20px;
   font-size: 14px;
   text-align: left;
 
@@ -39,7 +38,7 @@ const Bottom = styled.footer`
   justify-content: flex-start;
   text-align: left;
   gap: 10px;
-  padding: 20px 0;
+  padding-top: 20px;
 
   color: #979797;
 
@@ -49,8 +48,12 @@ const Bottom = styled.footer`
 `;
 
 const DirectionsBox = styled.div`
-  display: flex;
+  width: 100%;
+`;
 
+const SubDirectionsBox = styled.div`
+  display: flex;
+  align-items: flex-start;
   width: 100%;
 `;
 
@@ -58,35 +61,36 @@ export default function Section5() {
   return (
     <Wrap className="inner sectionBox">
       <DirectionsBox>
-        <Top>
-          <span className="fointBlueText">로건스톤</span>
-          <br />
-          <span>찾아오시는길</span>
-        </Top>
-        <MapBox>
-          <KakaoMap />
-        </MapBox>
+        <SubDirectionsBox>
+          <Top>
+            <span className="fointBlueText">로건스톤</span>
+            <br />
+            <span>찾아오시는길</span>
+          </Top>
+          <MapBox>
+            <KakaoMap />
+          </MapBox>
+        </SubDirectionsBox>
+        <Bottom>
+          <div>
+            <h4>(주)로건스톤</h4>
+            <p>서울특별시 영등포구 국제금융로 20, 5층 B520호(여의도동, 율촌빌딩)</p>
+            <br />
+            <p>사업자등록번호: 564-81-01633</p>
+            <p>전화번호: 000-0000-0000</p>
+            <p>팩스번호: 0504-498-1998</p>
+          </div>
+          <div>
+            <h4>고객지원</h4>
+            <br />
+            <ul>
+              <ol>도움말 및 자료</ol>
+              <ol>서비스 및 플랜</ol>
+              <ol>커뮤니티</ol>
+            </ul>
+          </div>
+        </Bottom>
       </DirectionsBox>
-
-      <Bottom>
-        <div>
-          <h4>(주)로건스톤</h4>
-          <p>서울특별시 영등포구 국제금융로 20, 5층 B520호(여의도동, 율촌빌딩)</p>
-          <br />
-          <p>사업자등록번호: 564-81-01633</p>
-          <p>전화번호: 000-0000-0000</p>
-          <p>팩스번호: 0504-498-1998</p>
-        </div>
-        <div>
-          <h4>고객지원</h4>
-          <br />
-          <ul>
-            <ol>도움말 및 자료</ol>
-            <ol>서비스 및 플랜</ol>
-            <ol>커뮤니티</ol>
-          </ul>
-        </div>
-      </Bottom>
     </Wrap>
   );
 }
