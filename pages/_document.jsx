62b227bba0df0e613397344519a7a2110e1b2ad2
chjfrom <chjfromn@gmail.com>
2024-03-05 13:56:26 +0900

    meta tag 설정

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>Logan Stone</title>
+        <meta name="description" content="Logan Stone 회사 홈페이지" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
